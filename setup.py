bb035d7bd3c973512526215733b06a77d8ef0bf3
VeritasYin <veritaspotter@gmail.com>
2021-06-22 14:47:01 -0400

    Initial upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('gcom_acc',
+                    sources = ['gcom_acc.c'])
+
+setup (name = 'GComAcc',
+       version = '1.0',
+       description = 'This is a package for accelerated graph operations.',
+       ext_modules = [module1])
