3ceb09f3f6a9669ad471dc730ce4cb356e184172
HarrisYin <veritaspotter@gmail.com>
2023-01-29 23:40:15 -0500

    Refactor code base & fix memory issue for large samples
    - optimize functions for sampling query batches and walks
    - rename functions
    - change dynamic allocation of array to malloc()

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup, Extension
 import numpy
 
-module1 = Extension('subg_acc',
-                    sources = ['subg_acc.c'],
-                    extra_compile_args=['-fopenmp'],
-                    extra_link_args=['-lgomp'],
-                    include_dirs=[numpy.get_include()])
+module = Extension('subg_acc',
+                   sources=['subg_acc.c'],
+                   extra_compile_args=['-fopenmp'],
+                   extra_link_args=['-lgomp'],
+                   include_dirs=[numpy.get_include()])
 
-setup (name = 'SubGAcc',
-       version = '2.0',
-       description = 'This is an extension library based on C and openmp for accelerating subgraph operations.',
-       ext_modules = [module1])
+setup(name='SubGAcc',
+      version='2.1',
+      description='This is an extension library based on C and openmp for accelerating subgraph operations.',
+      ext_modules=[module],
+      zip_safe=False
+      )
